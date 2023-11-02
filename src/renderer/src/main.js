8197f8504679df23e9fb95f8f5503dab03638f03
Bioninja01 <vinsonjames02@gmail.com>
2023-11-02 02:43:54 -0400

    this is the init of building a vue electron app

---
+++ src/renderer/src/main.js
@@ -0,0 +1,13 @@
+import devtools from '@vue/devtools'
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+app.mount('#app')
+
+if (process.env.NODE_ENV === 'development') {
+  devtools.connect()
+}
